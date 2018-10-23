a831fd3f96fed957c358585af6b7c937b55545b3
susuman <44407635+susuman@users.noreply.github.com>
2018-10-23 19:36:20 +0300

    Update program.js

--- program.js
+++ program.js
@@ -1,6 +1,8 @@
 function modifica()
 {
   document.getElementById("id_salut").innerHTML = "Hello";
+  document.getElementById("id_salut").innerHTML = "Salut";
+  document.getElementById("id_img").src = "bass.jpg";
 
 
 }
