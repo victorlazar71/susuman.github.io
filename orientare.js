b34dc883b840b092f18c811743c947c0356f605d
susuman <44407635+susuman@users.noreply.github.com>
2018-11-06 18:48:19 +0200

    Update orientare.js

--- orientare.js
+++ orientare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version : 2018.11.06.3";
+document.getElementById("id_logic_version").innerHTML = "Logic version : 2018.11.06.4";
 
 window.addEventListener("deviceorientation", la_schimbare_gyro);
 
@@ -13,6 +13,8 @@ var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
   
   var R = 50;
+  
+  context.clearRect(0, 0, canvas.width, canvas.height);
 
 context.beginPath();
 context.arc(canvas.width / 2 + e.gamma / 90 * (canvas.width / 2 - R), canvas.height / 2 + e.beta / 90 * (canvas.height / 2 - R), R, 0, 2 * Math.PI);
