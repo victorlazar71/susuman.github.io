8409aa514974ef9c2aaeb93fcf74145cb3ca3fab
susuman <44407635+susuman@users.noreply.github.com>
2018-11-06 18:40:55 +0200

    Update orientare.js

--- orientare.js
+++ orientare.js
@@ -12,7 +12,7 @@ function la_schimbare_gyro(e)
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
   
-  var R = 10;
+  var R = 100;
 
 context.beginPath();
 context.arc(canvas.width / 2 + e.gamma / 90 * (canvas.width / 2 - R), canvas.height / 2 + e.beta / 90 * (canvas.height / 2 - R), R, 0, 2 * Math.PI);
