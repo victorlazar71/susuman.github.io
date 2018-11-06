a2f32ca851c3294a4ba6de327f87faa246662217
susuman <44407635+susuman@users.noreply.github.com>
2018-11-06 18:01:28 +0200

    Update orientare.js

--- orientare.js
+++ orientare.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version : 2018.11.06"
+document.getElementById("id_logic_version").innerHTML = "Logic version : 2018.11.06";
 
 window.addEventListener("deviceorientaion", la_schimbare_gyro);
 
