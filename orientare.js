75dfbf89bd7e0fe5d2daf27064c0b9a60c4b9b7d
susuman <44407635+susuman@users.noreply.github.com>
2018-11-06 18:18:16 +0200

    Update orientare.js

--- orientare.js
+++ orientare.js
@@ -7,4 +7,12 @@ function la_schimbare_gyro(e)
   document.getElementById("id_x").innerHTML = e.beta;
   document.getElementById("id_y").innerHTML = e.gamma;
   document.getElementById("id_z").innerHTML = e.alpha;
-  }
+  
+
+var canvas = document.getElementById("id_canvas");
+var context = canvas.getContext("2d");
+
+context.beginPath();
+context.arc(canvas.width / 2, canvas.height / 2, 30, 0, 2 * Math.PI);
+context.stroke();
+}
