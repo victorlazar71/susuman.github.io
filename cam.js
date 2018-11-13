3bea8e1f8181ba85fa5297bb26dec1b6bc675b02
susuman <44407635+susuman@users.noreply.github.com>
2018-11-13 19:20:35 +0200

    Update cam.js

--- cam.js
+++ cam.js
@@ -1,9 +1,12 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.13.1";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.11.13.2";
+
+document.getElementById("id_video").addEventListener("mousedown", on_touch);
+document.getElementById("id_video").addEventListener("touchstart", on_touch);
 
 
 function on_OK(stream)
 {
-  document.getElementById("id_video").srcObject = stream;
+  document.getElementById("id_video").srcObject = stream; //preluare si redare video
   
 }
 
@@ -21,4 +24,13 @@ function start()
   var p = {audio:true, video:true};
   navigator.mediaDevices.getUserMedia(p).then(on_OK).catch(on_error);
 
+}
+
+function on_touch(e)
+{
+  var canvas = document.getElementById("id_canvas");
+  var context = document.getContext("2d");
+  
+  var video = document.getElementById("id_video");
+  context.drowImage(video, 0, 0);
 }
