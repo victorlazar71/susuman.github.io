2a049b31f0bfbb2f264d289007db1d460a3365f7
susuman <44407635+susuman@users.noreply.github.com>
2018-11-27 19:18:49 +0200

    Update desenare.js

--- desenare.js
+++ desenare.js
@@ -1,9 +1,11 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.11.20.13";
+document.getElementById("id_logic_version").innerHTML = 
+		"Logic version = 2018.11.27.4";
 		
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch);
 canvas.addEventListener("touchmove", on_touch_move);
 canvas.addEventListener("touchend", on_touch_end);
+
 
 var rect = canvas.getBoundingClientRect();
 //---------------------------------
@@ -14,6 +16,7 @@ var rect = canvas.getBoundingClientRect();
 
 var last_position_array = [];
 
+//---------------------------------
 function on_touch(e)
 {
 	e.preventDefault();
@@ -21,18 +24,18 @@ function on_touch(e)
 	for (var i = 0; i < e.changedTouches.length; i++){
 		var context = canvas.getContext("2d");
 		context.beginPath();
-		context.lineWidth = 1;///////////
+		context.lineWidth = 1;
 		context.arc(e.changedTouches.item(i).pageX - rect.left,
 					e.changedTouches.item(i).pageY - rect.top,
 					10,
 					0, 2 * Math.PI
 					);
 		context.stroke();
-		var last_position = {x:0, y:0, id:0};
+		var last_position = {x: 0, y: 0, id: 0};
 		last_position.x = e.changedTouches.item(i).pageX;		
-		last_pisition.y = e.changedTouches.item(i).pageY;
-		last_pisition.id = e.changedTouches.item(i).identifier;
-		past_position_array.push(last_position);
+		last_position.y = e.changedTouches.item(i).pageY;
+		last_position.id = e.changedTouches.item(i).identifier;
+		last_position_array.push(last_position);
 	}
 }
 //---------------------------------
@@ -49,17 +52,16 @@ function on_touch_move(e)
 					10,
 					0, 2 * Math.PI
 					);
-		
-		context.stroke();
+		context.stroke();			
+					
 		context.beginPath();
+		context.lineWidth = 20;		
+		var j = 0;
 		
-		context.lineWidth = 20;	
-		
-		var j =0;
-		for(; j < last_position_array.lenght; j++)
-			if(last_position_array[j].id == e.changedTouches.item(i).identifier)
+		for (; j < last_position_array.length; j++)
+			if (last_position_array[j].id == e.changedTouches.item(i).identifier)
 				break;
-		
+			
 		context.moveTo(last_position_array[j].x - rect.left, last_position_array[j].y - rect.top);
 		context.lineTo(e.changedTouches.item(i).pageX - rect.left, 
 						e.changedTouches.item(i).pageY - rect.top);
@@ -70,18 +72,16 @@ function on_touch_move(e)
 	}	
 }
 //---------------------------------
-
 function on_touch_end(e)
 {
-	for (var i = 0; i < e.changedTouches.length; i++)
-	{
-
-	var j =0;
-	for(; j < last_position_array.lenght; j++)
-	if(last_position_array[j].id == e.changedTouches.item(i).identifier)
-	break;
+	for (var i = 0; i < e.changedTouches.length; i++){
+		var j = 0;
 		
-	last_position_array.splice(j,1);
-	}
-	
+		for (; j < last_position_array.length; j++)
+			if (last_position_array[j].id == e.changedTouches.item(i).identifier)
+				break;
+		
+		last_position_array.splice(j, 1);
+	}	
 }
+//---------------------------------
