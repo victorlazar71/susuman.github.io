57c1502102b8481f38c7df00571e64797b899c6c
susuman <44407635+susuman@users.noreply.github.com>
2018-12-04 17:48:08 +0200

    Update synth.js

--- synth.js
+++ synth.js
@@ -2,7 +2,7 @@ document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.12
 
 function afiseaza_voci()
 {
-	var synth = window.speechSunthesis;
+	var synth = window.speechSynthesis;
 	var lista_voci = synth.getVoices();
 	
 	for(var i==0; i < lista_voci.lenght; i++)
