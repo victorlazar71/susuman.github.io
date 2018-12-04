8d02794366900856dde95b28a9d7acc0957e428e
susuman <44407635+susuman@users.noreply.github.com>
2018-12-04 18:27:06 +0200

    Update synth.js

--- synth.js
+++ synth.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.12.04.5";
+document.getElementById("id_logic_version").innerHTML = "Logic version = 2018.12.04.6";
 
 var synth = window.speechSynthesis;
 function afiseaza_voci()
@@ -18,7 +18,7 @@ function vorbeste()
 {
     var enunt = new SpeechSynthesisUtterance();
     enunt.text = document.getElementById("id_text").value;
-    enunt.lang = "en-US";
+    enunt.lang = "ja-JP";
     synth.speak(enunt);
 	
 	
