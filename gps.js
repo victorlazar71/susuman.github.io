3011ffddd0cb62604726706955a4bb530ea54254
susuman <44407635+susuman@users.noreply.github.com>
2018-12-11 19:07:54 +0200

    Add files via upload

---
+++ gps.js
@@ -0,0 +1,18 @@
+document.getElementById("id_logic_version").innerHTML = "Logic version 2018.12.11.0";
+
+function get_position()
+{
+	navigator.geolocation.getCurrentPosition(on_gps_ok, on_gps_error);
+}
+
+function on_gps_ok(e)
+{
+	document.getElementById("id_lat").innerHTML = e.coords.latitude;
+	document.getElementById("id_long").innerHTML = e.coords.longitude;
+	document.getElementById("id_acc").innerHTML = e.coords.accuracy;
+}
+
+function on_gps_error(e)
+{
+	alert("eroare GPS");
+}
