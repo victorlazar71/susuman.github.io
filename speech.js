8f014d1b0253ca7e4f55a30d3f18df8fe538204d
susuman <44407635+susuman@users.noreply.github.com>
2018-12-11 18:31:27 +0200

    Create speech.js

---
+++ speech.js
@@ -0,0 +1,21 @@
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2018.12.11.0";
+
+var speech = new webkitSpeechRecognition();
+speech.onresult = on_result;
+speech.onspeechend = on_speech_ebnd;
+speech.lang = "en-US";
+
+function speack()
+{
+  speech.start();
+}
+
+function on_speech_end()
+{
+  speech.stop();
+}
+
+function on_result()
+{
+  document.getElementById("id_text").innerHTML = e.results[0][0].transcript + "("+ e.results[0][0].confidence + ")";
+  }
