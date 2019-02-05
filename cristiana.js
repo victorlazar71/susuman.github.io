9d700f00c774075cc17187f04d510529faaae4f0
susuman <44407635+susuman@users.noreply.github.com>
2019-02-05 17:00:32 +0200

    Add files via upload

--- cristiana.js
+++ cristiana.js
@@ -1,5 +1,5 @@
 document.getElementById("id_logic_version").innerHTML = 
-		"Logic version = 2018.12.04.1";
+		"Logic version = 2000.00.00.1";
 		
 var canvas = document.getElementById("id_canvas");
 canvas.addEventListener("touchstart", on_touch);
