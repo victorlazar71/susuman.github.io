81ac8d5c9c8b0ea6785ce231cf6a114bb1dac7c5
susuman <44407635+susuman@users.noreply.github.com>
2019-02-06 22:31:20 +0200

    Add files via upload

--- examen.js
+++ examen.js
@@ -1,12 +1,16 @@
-document.getElementById("canvas").innerHTML = "Logic version: 2000.00.00.1";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2000.00.00.1";
 
 var canvas = document.getElementById("canvas");
 
-//canvas.addEventListener("touchstart", on_touch);
+canvas.addEventListener("touchstart", on_touch);
 //canvas.addEventListener("touchmove", on_touch_move);
 //canvas.addEventListener("touchend", on_touch_end);
 
-c = canvas.getContext("2d");
-x=0, y=0, width=10, height=10;
+function on_touch(e)
+{
+	c = canvas.getContext("2d");
+	x=0, y=0, width=10, height=10;
 
 c.fillRect(x, y, width, height);
+
+}
