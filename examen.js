a5c59f04d4b44fa98811cbe25dab8a7008d01a62
susuman <44407635+susuman@users.noreply.github.com>
2019-02-06 23:44:50 +0200

    Add files via upload

--- examen.js
+++ examen.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2000.00.00.6";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2000.00.00.7";
 
 var svg = document.getElementById("id_svg");
 
@@ -12,10 +12,10 @@ var svg_rect = svg.getBoundingClientRect();
 
 function genereaza_culoare()
 {
-	var sir = "#00FF00";
-	//var litere = "0123456789ABCDEF";
+	var sir = "#";
+	var litere = "0123456789ABCDEF";
 	for (var i = 0; i < 6; i++)
-		sir //+= litere[Math.ceil(Math.random() * 16)];
+		sir += litere[Math.ceil(Math.random() * 16)];
 	return sir;
 }
 
@@ -24,20 +24,13 @@ function on_touch_svg(e)
 	
 	for (var i = 0; i < e.changedTouches.length; i++)
 	{
-		var cub = document.createElementNS("http://www.w3.org/2000/svg", "rect");
-		//var position = {x: 0, y: 0, id: 0};
-		//position.id = e.changedTouches.item(i).identifier;
-		cub.color = genereaza_culoare();
-		//context.beginPath();
-		
+		var cub = document.createElementNS("http://www.w3.org/2000/svg", "rect");	
 		cub.setAttribute("x", e.changedTouches[i].pageX);  //atributele cercului cu valorile din paranteze
 		cub.setAttribute("y", e.changedTouches[i].pageY - svg_rect.top);
 		cub.setAttribute("width", 100);
 		cub.setAttribute("height", 100);
 		svg.appendChild(cub);  // adaugare in svg ca si "copil" (subelement)
-		context.strokeStyle = cub.color;
-		context.fillStyle = cub.color;
-		
+		c.fillStyle = genereaza_culoare();
 
 }
 }
