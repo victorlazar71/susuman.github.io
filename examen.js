afaad01137cbcb0bb23eb7e57f3472d236dfb32d
susuman <44407635+susuman@users.noreply.github.com>
2019-02-07 00:34:01 +0200

    Add files via upload

--- examen.js
+++ examen.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2000.00.00.3";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2000.00.00.0";
 
 var svg = document.getElementById("id_svg");
 
@@ -9,8 +9,6 @@ svg.addEventListener("mousedown", on_touch_svg);
 //canvas.addEventListener("touchend", on_touch_end);
 
 var svg_rect = svg.getBoundingClientRect();
-
-var col = {r:0, g:0, b:0};
 
 function genereaza_culoare()
 {
@@ -23,7 +21,7 @@ function genereaza_culoare()
 
 function on_touch_svg(e)
 {
-	
+	var culoare = genereaza_culoare();
 	for (var i = 0; i < e.changedTouches.length; i++)
 	{
 		var cub = document.createElementNS("http://www.w3.org/2000/svg", "rect");	
@@ -32,6 +30,7 @@ function on_touch_svg(e)
 		cub.setAttribute("width", 120);
 		cub.setAttribute("height", 50);
 		svg.appendChild(cub);  // adaugare in svg ca si "copil" (subelement)
+		cub.fill = culoare;
 
 
 
