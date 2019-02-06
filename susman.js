ea6e9d12155067e03323a471fbda5cf6386002fb
susuman <44407635+susuman@users.noreply.github.com>
2019-02-06 20:16:17 +0200

    Add files via upload

--- susman.js
+++ susman.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version = 2000.00.00.2";
+document.getElementById("id_logic_version").innerHTML = "Logic version = 2000.00.00.3";
 		
 var canvas,
     context,
