d53eb864f54c927e7b2249efe959142874f4ce91
susuman <44407635+susuman@users.noreply.github.com>
2019-02-06 17:37:46 +0200

    Add files via upload

--- susman.js
+++ susman.js
@@ -1,2 +1,22 @@
 document.getElementById("id_logic_version").innerHTML = " Logic version 2019.05.02.0";
 
+var svg = document.getElementById("id_svg");
+//var stea = document.getElementById("id_circle");
+
+svg.addEventListener("touchstart", on_touch_svg);
+svg.addEventListener("mousedown", on_touch_svg);
+
+var svg_rect = svg.getBoundingClientRect();
+
+function on_touch_svg(e)
+{
+	for (var i = 0; i < e.changedTouches.length; i++)
+	{
+		var stea = document.createElementNS("http://www.w3.org/2000/svg", "polygon"); //creare element cerc in cazul nostru
+		stea.setAttribute("points", e.changedTouches[i].pageX);  //atributele cercului cu valorile din paranteze
+		stea.setAttribute("points", e.changedTouches[i].pageY - svg_rect.top);
+		//stea.setAttribute("points", 20);
+		svg.appendChild(stea);  // adaugare in svg ca si "copil" (subelement)
+
+}
+}
