87e817aa2345dd4b6f0097edab040a80945a001e
susuman <44407635+susuman@users.noreply.github.com>
2019-02-06 19:58:33 +0200

    Add files via upload

--- susman.js
+++ susman.js
@@ -1,61 +1,65 @@
-document.getElementById("id_logic_version").innerHTML = " Logic version 2019.05.02.4";
+var canvas,
+    context,
+    dragging = false,
+    dragStartLocation,
+    snapshot;
 
-var canvas, context, dragging = false, dragStartLocation;
 
-//canvas.addEventListener("touchstart", on_touch);
-//canvas.addEventListener("touchmove", on_touch_move);
-//canvas.addEventListener("touchend", on_touch_end);
+function getCanvasCoordinates(event) {
+    var x = event.clientX - canvas.getBoundingClientRect().left,
+        y = event.clientY - canvas.getBoundingClientRect().top;
 
-function getCanvasCoordinates(e)
-{
-	var x = e.clientX - canvas.getBoundingClientRect().left,
-		y = e.clientY - canvas.getBoundingClientRect().top;
-		
-		return {x: x, y: y};
+    return {x: x, y: y};
 }
 
-function drawLine(position)
-{
-	context.beginPath();
-	context.moveTo(dragStartLocation.x, dragStartLocation.y);
-	context.lineTo(position.x, position.y);
-	context.stroke();
+function takeSnapshot() {
+    snapshot = context.getImageData(0, 0, canvas.width, canvas.height);
 }
 
-function dragStart(e)
-{
-	dragging = true;
-	dragStartLocation = getCanvasCoordinates(e);
+function restoreSnapshot() {
+    context.putImageData(snapshot, 0, 0);
 }
 
-function drag(e)
-{
-	var position;
-	if(dragging == true)
-	{
-		position = getCanvasCoordinates(e);
-		drawLine(position);
-	}
+
+function drawLine(position) {
+    context.beginPath();
+    context.moveTo(dragStartLocation.x, dragStartLocation.y);
+    context.lineTo(position.x, position.y);
+    context.stroke();
 }
 
-function dragStop(e)
-{
-	dragging = false;
-	var position = getCanvasCoordinates(e);
-	drawLine(position);
+function dragStart(event) {
+    dragging = true;
+    dragStartLocation = getCanvasCoordinates(event);
+    takeSnapshot();
 }
 
-function init()
-{
-	canvas = document.getElementById("id_canvas");
-	context = canvas.getContext("2D");
-	context.stokeStyle = "green";
-	context.lineWidth = 6;
-	context.lineCap = "round";
-	
-	canvas.addEventListener("on_touch", dragStart, false);
-	canvas.addEventListener("on_touch_move", drag, false);
-	canvas.addEventListener("on_touch_stop", dragStop, false);
-	
-	//window.addEventListener("load", init, false);
+function drag(event) {
+    var position;
+    if (dragging === true) {
+        restoreSnapshot();
+        position = getCanvasCoordinates(event);
+        drawLine(position);
+    }
 }
+
+function dragStop(event) {
+    dragging = false;
+    restoreSnapshot();
+    var position = getCanvasCoordinates(event);
+    drawLine(position);
+}
+
+function init() {
+    canvas = document.getElementById("canvas");
+    context = canvas.getContext('2d');
+    context.strokeStyle = 'purple';
+    context.lineWidth = 6;
+    context.lineCap = 'round';
+
+    canvas.addEventListener('mousedown', dragStart, false);
+    canvas.addEventListener('mousemove', drag, false);
+    canvas.addEventListener('mouseup', dragStop, false);
+}
+
+window.addEventListener('load', init, false);
