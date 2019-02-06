8c2698f391b111ba23564df6b2d153c4e126ab43
susuman <44407635+susuman@users.noreply.github.com>
2019-02-06 23:34:54 +0200

    Add files via upload

--- examen.js
+++ examen.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2000.00.00.4";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2000.00.00.3";
 
 var svg = document.getElementById("id_svg");
 
@@ -26,17 +26,17 @@ function on_touch_svg(e)
 	{
 		var cub = document.createElementNS("http://www.w3.org/2000/svg", "rect");
 		var position = {x: 0, y: 0, id: 0};
-		//position.id = e.changedTouches.item(i).identifier;
-		//position.color = genereaza_culoare();
-		//context.beginPath();
-		context.strokeStyle = position.color;
-		context.fillStyle = position.color;
+		position.id = e.changedTouches.item(i).identifier;
+		position.color = genereaza_culoare();
+		context.beginPath();
+		
 		cub.setAttribute("x", e.changedTouches[i].pageX);  //atributele cercului cu valorile din paranteze
 		cub.setAttribute("y", e.changedTouches[i].pageY - svg_rect.top);
 		cub.setAttribute("width", 100);
 		cub.setAttribute("height", 100);
 		svg.appendChild(cub);  // adaugare in svg ca si "copil" (subelement)
-		
+		context.strokeStyle = position.color;
+		context.fillStyle = position.color;
 		context.fill();
 		context.stroke();
 		
