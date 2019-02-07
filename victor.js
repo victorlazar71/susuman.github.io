85aaa3bd336f5a8b58a549926a3fba08f05e8f43
susuman <44407635+susuman@users.noreply.github.com>
2019-02-07 19:15:04 +0200

    Update victor.js

--- victor.js
+++ victor.js
@@ -1,13 +1,13 @@
-document.getElementById("id_logic_version").innerHTML = "Logic: 2019.01.08.2";
+document.getElementById("id_logic_version").innerHTML = "Logic: 2019.01.08.3";
 var svg = document.getElementById("id_svg");
 var patrat = document.getElementById("id_patrat");
 
-rect.addEventListener("touchmove", on_touch_move);
+patrat.addEventListener("touchmove", on_touch_move);
 var svg_rect = svg.getBoundingClientRect();
 
 function on_touch_move(e)
 {
 	e.preventDefault();
-	cerc.setAttribute("x", e.changedTouches[0].pageX);
-	cerc.setAttribute("y", e.changedTouches[0].pageY - svg_rect.top);
+	patrat.setAttribute("x", e.changedTouches[0].pageX);
+	patrat.setAttribute("y", e.changedTouches[0].pageY - svg_rect.top);
 }
