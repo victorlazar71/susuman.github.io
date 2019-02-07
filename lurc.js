c7fa66bcd0aa0b0eaa0f6bdb46bbd9e75d3ba57e
susuman <44407635+susuman@users.noreply.github.com>
2019-02-07 22:41:19 +0200

    Update lurc.js

--- lurc.js
+++ lurc.js
@@ -1,10 +1,9 @@
-document.getElementById("id_logic_version").innerHTML = "Logic: 2019.02.11.3";
+document.getElementById("id_logic_version").innerHTML = "Logic: 2019.02.11.0";
 var svg = document.getElementById("id_svg");
 var cerc = document.getElementById("id_circle");
 
 cerc.addEventListener("touchstart", on_touch);
 var svg_rect = svg.getBoundingClientRect();
-<audio id="audio" src="http://www.soundjay.com/button/beep-07.wav" autoplay="false" ></audio>
 var sound = document.getElementById("audio");
 
 function on_touch(e)
