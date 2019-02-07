f723f1f76c6ccee0c1cbc7c4727cd1fef2bad5a4
susuman <44407635+susuman@users.noreply.github.com>
2019-02-07 21:23:26 +0200

    Add files via upload

--- lurc.js
+++ lurc.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_versio").innerHTML = "Logic Version 2019.02.11.0";
+document.getElementById("id_logic_version").innerHTML = "Logic Version 2019.02.11.0";
 
 var svg = document.getElementById("id_svg");
 
