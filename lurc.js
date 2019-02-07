62024a4cec3038c4fb13feaeb2178e769e76eaae
susuman <44407635+susuman@users.noreply.github.com>
2019-02-07 22:26:41 +0200

    Add files via upload

--- lurc.js
+++ lurc.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic: 2019.02.11.2";
+document.getElementById("id_logic_version").innerHTML = "Logic: 2019.02.11.3";
 var svg = document.getElementById("id_svg");
 var cerc = document.getElementById("id_circle");
 
@@ -9,7 +9,7 @@ function on_touch(e)
 {
 	var x = "#00ff00";
 	e.preventDefault();
-	cerc.setAttribute("cx", e.changedTouches[0].pageX);
-	cerc.setAttribute("cy", e.changedTouches[0].pageY - svg_rect.top);
+	//cerc.setAttribute("cx", e.changedTouches[0].pageX);
+	//cerc.setAttribute("cy", e.changedTouches[0].pageY - svg_rect.top);
 	cerc.setAttribute("fill", x);
 }
