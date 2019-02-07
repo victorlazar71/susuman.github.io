fe48283d6084c8b3f5661d1b90dddbbe8c6329dc
susuman <44407635+susuman@users.noreply.github.com>
2019-02-07 12:36:59 +0200

    Update examen.js

--- examen.js
+++ examen.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2000.00.00.5";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2000.00.00.6";
 
 var svg = document.getElementById("id_svg");
 
@@ -31,7 +31,7 @@ function on_touch_svg(e)
 		cub.setAttribute("width", 120);
 		cub.setAttribute("height", 50);
 		cub.setAttribute("fill", color);
+		cub.setAttribute("stroke", color);
 		svg.appendChild(cub);  // adaugare in svg ca si "copil" (subelement)
-		//svg.fillStyle = "blue";
 }
 }
