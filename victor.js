675a5be3d909b4334e5b12ce5f84d36a8a783efe
susuman <44407635+susuman@users.noreply.github.com>
2019-02-07 19:12:48 +0200

    Add files via upload

--- victor.js
+++ victor.js
@@ -2,7 +2,7 @@ document.getElementById("id_logic_version").innerHTML = "Logic: 2019.01.08.2";
 var svg = document.getElementById("id_svg");
 var patrat = document.getElementById("id_patrat");
 
-cerc.addEventListener("touchmove", on_touch_move);
+rect.addEventListener("touchmove", on_touch_move);
 var svg_rect = svg.getBoundingClientRect();
 
 function on_touch_move(e)
