7b1b484f83c305dcfea463f280af690c24fdce9a
susuman <44407635+susuman@users.noreply.github.com>
2019-02-07 19:06:50 +0200

    Add files via upload

---
+++ victor.js
@@ -0,0 +1,13 @@
+document.getElementById("id_logic_version").innerHTML = "Logic: 2019.01.08.2";
+var svg = document.getElementById("id_svg");
+var patrat = document.getElementById("id_patrat");
+
+cerc.addEventListener("touchmove", on_touch_move);
+var svg_rect = svg.getBoundingClientRect();
+
+function on_touch_move(e)
+{
+	e.preventDefault();
+	cerc.setAttribute("x", e.changedTouches[0].pageX);
+	cerc.setAttribute("y", e.changedTouches[0].pageY - svg_rect.top);
+}
