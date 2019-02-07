701561a7654bd599d969e929c8fc99c3ebaada46
susuman <44407635+susuman@users.noreply.github.com>
2019-02-07 22:00:10 +0200

    Add files via upload

--- lurc.js
+++ lurc.js
@@ -1,14 +1,9 @@
-document.getElementById("id_logic_version").innerHTML = "Logic Version 2019.02.11.3";
+document.getElementById("id_logic_version").innerHTML = "Logic Version 2019.02.11.1";
 
 var svg = document.getElementById("id_svg");
-var cerc1 = document.getElementById("id_cerc1");
-//var cerc2 = document.getElementById("id_cerc2");
-//var cerc3 = document.getElementById("id_cerc3");
+var cerc = document.getElementById("id_cerc");
 
-
-//cerc1.addEventListener("touchstart", on_touch);
-cerc1.addEventListener("touchmove", on_touch_move);
-//cerc3.addEventListener("touchstart", on_touch);
+cerc.addEventListener("touchmove", on_touch_move);
 var svg_rect = svg.getBoundingClientRect();
 
 function genereaza_culoare()
@@ -24,8 +19,8 @@ function on_touch_move()
 {
 		//var x = genereaza_culoare();
 		e.preventDefault();
-		cerc1.setAttribute("cx", e.changedTouches[0].pageX);
-		cerc1.setAttribute("cy", e.changedTouches[0].pageY - svg_rect.top);
+		cerc.setAttribute("cx", e.changedTouches[0].pageX);
+		cerc.setAttribute("cy", e.changedTouches[0].pageY - svg_rect.top);
 		//cerc1.setAttribute("fill", x);
 
 }
