a368c8a0e891323202fc2858299655631c9c5948
susuman <44407635+susuman@users.noreply.github.com>
2019-02-07 19:18:10 +0200

    Update victor.js

--- victor.js
+++ victor.js
@@ -1,13 +1,24 @@
-document.getElementById("id_logic_version").innerHTML = "Logic: 2019.01.08.3";
+document.getElementById("id_logic_version").innerHTML = "Logic: 2019.01.08.4";
 var svg = document.getElementById("id_svg");
 var patrat = document.getElementById("id_patrat");
 
 patrat.addEventListener("touchmove", on_touch_move);
 var svg_rect = svg.getBoundingClientRect();
 
+function genereaza_culoare()
+{
+	var sir = "#";
+	var litere = "0123456789ABCDEF";
+	for (var i = 0; i < 6; i++)
+		sir += litere[Math.ceil(Math.random() * 16)];
+	return sir;
+}
+
 function on_touch_move(e)
 {
+	var a = genereaza_culoare();
 	e.preventDefault();
 	patrat.setAttribute("x", e.changedTouches[0].pageX);
 	patrat.setAttribute("y", e.changedTouches[0].pageY - svg_rect.top);
+	patrat.setAttribute("fill", a);
 }
