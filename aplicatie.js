63a54b1c7ded1bcc38a972737c38e525182aadbd
susuman <44407635+susuman@users.noreply.github.com>
2019-02-08 19:24:09 +0200

    Add files via upload

--- aplicatie.js
+++ aplicatie.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic Version: 2000.00.00.2";
+document.getElementById("id_logic_version").innerHTML = "Logic Version: 2000.00.00.3";
 
 var svg = document.getElementById("svg");
 var picture = document.getElementById("stea");
@@ -8,7 +8,11 @@ svg.addEventListener("touchstart", init);
 
 function init(e)
 {
-	picture;
+	e.preventDefault();
+	
+	for (var i = 0; i < e.changedTouches.length; i++)
+	{
+		picture;
 	
 }
-
+}
