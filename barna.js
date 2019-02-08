ba63422d11d6052d32b8b7b1179aae3154f5b2ab
susuman <44407635+susuman@users.noreply.github.com>
2019-02-08 02:48:23 +0200

    Add files via upload

--- barna.js
+++ barna.js
@@ -1,5 +1,8 @@
-document.getElementById("id_logic_version").innerHTML = "Logic Version 2000.00.00.3";
+document.getElementById("id_logic_version").innerHTML = "Logic Version 2000.00.00.5";
 
+	var c2 = document.getElementById("canvas");
+    var ctx = canvas.getContext("2d");
+	
 function genereaza_culoare()
 {
 	var sir = "#";
@@ -8,29 +11,27 @@ function genereaza_culoare()
 		sir += litere[Math.ceil(Math.random() * 16)];
 	return sir;
 }
-
-var c2 = document.getElementById("canvas");
-
-    var c2_context = c2.getContext("2d");
+	
     function blue_square() 
 	{ 
-	var a = genereaza_culoare();
-      c2_context.fillStyle = a;
-      c2_context.fillRect(100, 100, 100, 100);
+	  var a = genereaza_culoare();
+      ctx.fillStyle = a;
+      ctx.fillRect(100, 100, 100, 100);
+	  ctx.stroke();
    }
 
    
    function red_stroke() 
    { 
-   var z = genereaza_culoare();
-   c2_context.lineWidht = 100;
-      c2_context.strokeStyle = z;
-      c2_context.strokeRect(95, 95, 110, 110);
-	  c2_context.stroke();
+	  var z = genereaza_culoare();
+	  ctx.lineWidht = 100;
+      ctx.strokeStyle = z;
+      ctx.strokeRect(95, 95, 110, 110);
+	  ctx.stroke();
 	  
    }
    
    function clear_rect() 
    { 
-      c2_context.clearRect(90, 90, 120, 120);
+      ctx.clearRect(90, 90, 120, 120);
    }
