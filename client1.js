fd1362975011a0fd5a614718858f3efc078703aa
susuman <44407635+susuman@users.noreply.github.com>
2019-02-08 13:50:55 +0200

    Add files via upload

--- client1.js
+++ client1.js
@@ -1,25 +1,16 @@
-document.getElementById("id_logic_version").innerHTML = "Logic Version 2000.00.00.4";
+document.getElementById("id_logic_version").innerHTML = "Logic Version 2000.00.00.0";
 
-svg.addEventListener("touchstart", on_touch);
 
-var svg_rect = svg.getBoundingClientRect();
 
-function on_touch(e)
+function desenare(x, y)
 {
-	for (var i = 0; i < e.changedTouches.length; i++)
-	{
 	var canvas = document.getElementById("canvas");
 	var desen = canvas.getContext("2d");
-	desen.beginPath();
-	desen.lineWidth = 1;
 	desen.save();
 	desen.clearRect(0, 0, 600, 600);
 	desen.fillStyle = "pink";
-	desen.fillRect(20, 20, 100, 100);
+	desen.fillRect(x, 20, 100, 100);
 	desen.restore()
-	desen.fill();
-	desen.stroke();
-	//x+=5
-	//var loopTimer = setTimeout("on_touch("+e+")", 20);
-}
-}
+	x+=5
+	var loopTimer = setTimeout("desenare("+x+", "+y+")", 20);
+	}
