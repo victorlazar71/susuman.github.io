3aa9d218c8b8b00680877b1b8617dd3d4fc41012
susuman <44407635+susuman@users.noreply.github.com>
2019-02-08 02:00:01 +0200

    Add files via upload

--- barna.js
+++ barna.js
@@ -1,18 +1,18 @@
 document.getElementById("id_logic_version").innerHTML = "Logic Version 2000.00.00.0";
 
-var c2 = document.getElementById("c2");
+var c2 = document.getElementById("canvas");
 
     var c2_context = c2.getContext("2d");
-    function blue_square_2() { //Blue color square
+    function blue_square() { //Blue color square
       c2_context.fillStyle = "#00f";
       c2_context.fillRect(50, 50, 100, 100);
    }
    
-   function red_stroke_2() { //Red color edges
+   function red_stroke() { //Red color edges
       c2_context.strokeStyle = "#f00";
       c2_context.strokeRect(45, 45, 110, 110);
    }
    
-   function clear_rect_2() { //Clear all
+   function clear_rect() { //Clear all
       c2_context.clearRect(40, 40, 120, 120);
    }
