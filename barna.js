0ef4a3602978517182c16a03bc84034d65a3131a
susuman <44407635+susuman@users.noreply.github.com>
2019-02-08 02:04:01 +0200

    Add files via upload

--- barna.js
+++ barna.js
@@ -5,14 +5,18 @@ var c2 = document.getElementById("canvas");
     var c2_context = c2.getContext("2d");
     function blue_square() { //Blue color square
       c2_context.fillStyle = "#00f";
-      c2_context.fillRect(50, 50, 100, 100);
+      c2_context.fillRect(100, 100, 100, 100);
+	  c2_context.fillStyle = "#0f0";
+      c2_context.fillRect(100, 100, 100, 100);
+	  c2_context.fillStyle = "#f00";
+      c2_context.fillRect(100, 100, 100, 100);
    }
    
    function red_stroke() { //Red color edges
       c2_context.strokeStyle = "#f00";
-      c2_context.strokeRect(45, 45, 110, 110);
+      c2_context.strokeRect(95, 95, 110, 110);
    }
    
    function clear_rect() { //Clear all
-      c2_context.clearRect(40, 40, 120, 120);
+      c2_context.clearRect(90, 90, 120, 120);
    }
