6c11d41e1a1fe8210adac56115a2690fba1f4bb4
susuman <44407635+susuman@users.noreply.github.com>
2019-02-09 22:47:12 +0200

    Add files via upload

--- dan.js
+++ dan.js
@@ -1,4 +1,4 @@
-document.getElementById("id_logic_version").innerHTML = "Logic version: 2019.02.11.0";
+document.getElementById("id_logic_version").innerHTML = "Logic version: 2019.02.11.1";
 
 var svg = document.getElementById("id_svg");
 
