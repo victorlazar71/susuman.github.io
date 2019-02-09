4f703064621b3d0d9e21404e104c8b19d598fc4b
susuman <44407635+susuman@users.noreply.github.com>
2019-02-09 22:07:14 +0200

    Add files via upload

--- examen1.js
+++ examen1.js
@@ -3,11 +3,15 @@ document.getElementById("id_logic_version").innerHTML = "Logic: 2019.02.11.0";
 var svg = document.getElementById("id_svg");
 var site_html = document.getElementById("id_html");
 var site_java = document.getElementById("id_java");
-var DBS = document.getElementById("id_DBS");
+var monitorizare_gps= document.getElementById("id_NIXE");
+var service_auto = document.getElementById("id_NIXE");
+var verificare_alimentare = document.getElementById("id_NIXE");
 
 site_html.addEventListener("touchstart", on_touch_one);
 site_java.addEventListener("touchstart", on_touch_two);
-DBS.addEventListener("touchstart", on_touch_tree);
+monitorizare_gps.addEventListener("touchstart", on_touch_tree);
+service_auto.addEventListener("touchstart", on_touch_tree);
+verificare_alimentare.addEventListener("touchstart", on_touch_tree);
 
 var svg_rect = svg.getBoundingClientRect();
 
@@ -52,6 +56,36 @@ function on_touch_tree(e)
 		alert("ai apasat anulare");
 	}
 	
-window.open("https://screenrant.com/tag/dragonball/");
-alert("ai accesat pagina dragon ball");
+window.open("https://fise.i-track.ro/_layouts/login.aspx?ReturnUrl=%2f");
+alert("ai accesat pagina de monitorizare prin GPS a vehiculelor auto");
+}
+
+function on_touch_four(e)
+{
+		if(confirm("esti sigur ca vrei sa accesezi aceasta pagina??"))
+	{
+		alert("ai apasat OK");
+	}
+	else
+	{
+		alert("ai apasat anulare");
+	}
+	
+window.open("https://www.fleet1.ro/");
+alert("ai accesat pagina de programare la service auto sau pentru a comanda pise de schimb");
+}
+
+function on_touch_five(e)
+{
+		if(confirm("esti sigur ca vrei sa accesezi aceasta pagina??"))
+	{
+		alert("ai apasat OK");
+	}
+	else
+	{
+		alert("ai apasat anulare");
+	}
+	
+window.open("https://fleet.omv.com/FleetServicesProduction/Login.jsp");
+alert("ai accesat pagina de monitorizare a carburantului incarcat in masini");
 }
